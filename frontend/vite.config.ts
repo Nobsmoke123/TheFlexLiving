4edeedc4dd4cbc95f4d7a9e2d880825dc6a90b06
Nobsmoke123 <donaldakobundu@gmail.com>
2025-09-20 23:47:44 +0100

    fix: update API base URL and correct allowed hosts in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
     host: "::",
-    allowedHosts: ["theflexassessmentfrontend-production.up.railway.app"],
+    allowedHosts: ["theflexliving-frontend-production.up.railway.app"],
     port: 5000,
   },
   plugins: [react(), tailwindcss()],
